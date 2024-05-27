738fa941eb50a1fb990be480ec8c0d254fa1ed47
Javad Rafiei Asl <57322461+jasl1@users.noreply.github.com>
2024-05-26 21:18:05 -0400

    Create gec.py

---
+++ models/gec.py
@@ -0,0 +1,8 @@
+class GEC:
+    def __init__(self, model_path):
+        # Load your GEC model
+        pass
+
+    def check_grammatical_constraint(self, words, candidates, index):
+        # Your implementation
+        pass
