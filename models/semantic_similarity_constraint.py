07205263881b120692fbcd76378f7cfdeb0d7e30
Javad Rafiei Asl <57322461+jasl1@users.noreply.github.com>
2024-05-26 21:23:30 -0400

    Delete models/semantic_similarity_constraint.py

--- models/semantic_similarity_constraint.py
+++
@@ -1,8 +0,0 @@
-class Semantic_Similarity_Constraint:
-    def __init__(self, model, threshold):
-        self.model = model
-        self.threshold = threshold
-
-    def check_constraint(self, words, candidates, index):
-        sims = self.model.get_sims_at_index(words, candidates, index)
-        return [sim >= self.threshold for sim in sims]
