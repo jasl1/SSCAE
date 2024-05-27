5cd5bd7c851e200469e8dbc406f0aa21e781818a
Javad Rafiei Asl <57322461+jasl1@users.noreply.github.com>
2024-05-26 21:19:44 -0400

    Create infersent.py

---
+++ models/infersent.py
@@ -0,0 +1,11 @@
+import torch
+from models.SentEval.senteval import SentEval
+
+class INFERSENT:
+    def __init__(self, model_path, vocab_path, windows, device):
+        # Your implementation
+        pass
+
+    def get_sims_at_index(self, prev_words, candidates, index):
+        # Your implementation
+        pass
