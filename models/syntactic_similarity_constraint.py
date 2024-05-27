ed40fe553fd3c6c416bd5b234280268390695857
Javad Rafiei Asl <57322461+jasl1@users.noreply.github.com>
2024-05-26 21:23:40 -0400

    Delete models/syntactic_similarity_constraint.py

--- models/syntactic_similarity_constraint.py
+++
@@ -1,8 +0,0 @@
-class Syntactic_Similarity_Constraint:
-    def __init__(self, model, threshold):
-        self.model = model
-        self.threshold = threshold
-
-    def check_constraint(self, words, candidates, index):
-        diffs = self.model.get_probs_diff_at_index(words, candidates, index)
-        return [diff <= self.threshold for diff in diffs]
