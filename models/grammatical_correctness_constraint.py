0e2e9fd9f3a36ecabfa36cdb75b092edb1df37ba
Javad Rafiei Asl <57322461+jasl1@users.noreply.github.com>
2024-05-26 21:23:13 -0400

    Delete models/grammatical_correctness_constraint.py

--- models/grammatical_correctness_constraint.py
+++
@@ -1,7 +0,0 @@
-class Grammatical_Correctness_Constraint:
-    def __init__(self, model, error_threshold):
-        self.model = model
-        self.error_threshold = error_threshold
-
-    def check_constraint(self, words, candidates, index):
-        return self.model.check_grammatical_constraint(words, candidates, index)
