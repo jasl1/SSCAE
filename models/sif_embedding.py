112da828f52d8d2c145c98e89b0a8c902d5a0c44
Javad Rafiei Asl <57322461+jasl1@users.noreply.github.com>
2024-05-26 21:20:13 -0400

    Create sif_embedding.py

---
+++ models/sif_embedding.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+class SIF_embedding:
+    def __init__(self, embedding_path, words_to_frequencies_path):
+        # Your implementation
+        pass
+
+    def get_sims_at_index(self, prev_words, candidates, index):
+        # Your implementation
+        pass
